64d3a373547132448b8bbce5c5537b9d66684a2f
shreedhar_ganapathy <shreedhar_ganapathy@8864c36b-0a6f-c8da-9852-f1849ec27926>
2007-05-07 23:08:37 +0000

    added zone config and updates to left nav

---
+++ www/zone.js
@@ -0,0 +1,14 @@
+var zones = [
+	{
+		title: "Project Shoal",
+		href:  "https://shoal.dev.java.net/"
+	},
+	{
+		title: "GlassFish Community",
+		href:  "https://glassfish.dev.java.net/"
+	},
+	{
+		title: "Project Jxta",
+		href:  "http://www.jxta.org/"
+	}
+];
